d503603cacadba1e544b5c7e9a608cc482c5363d
xuhao199224 <409492932@qq.com>
2015-05-13 22:51:53 +0800

    implemented kMeans clustering algorithm and 3D scatter plot

--- Clustering/ProgramEntry.py
+++ Clustering/ProgramEntry.py
@@ -1,9 +1,7 @@
 __author__ = 'hao'
 
-
 from numpy import *
 import kMeans
-import os
 
 
 dataMat = mat(kMeans.loadDataSet('testSet2.txt'))
--- Clustering/kMeans.py
+++ Clustering/kMeans.py
@@ -89,34 +89,34 @@ def biKmeans(dataSet, k, distMeas=distEclud):
     return mat(centList), clusterAssment
 
 
-def clusterClubs(numClust=5):
-    datList = []
-    for line in open('places.txt').readlines():
-        lineArr = line.split('\t')
-        datList.append([float(lineArr[4]), float(lineArr[3])])
-    datMat = mat(datList)
-    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
-    fig = plt.figure()
-    rect = [0.1, 0.1, 0.8, 0.8]
-    scatterMarkers = ['s', 'o', '^', '8', 'p', 'd', 'v', 'h', '>', '<']
-    axprops = dict(xticks=[], yticks=[])
-    ax0 = fig.add_axes(rect, label='ax0', **axprops)
-    imgP = plt.imread('Portland.png')
-    ax0.imshow(imgP)
-    ax1 = fig.add_axes(rect, label='ax1', frameon=False)
-    for i in range(numClust):
-        ptsInCurrCluster = datMat[nonzero(clustAssing[:, 0].A == i)[0], :]
-        markerStyle = scatterMarkers[i % len(scatterMarkers)]
-        ax1.scatter(ptsInCurrCluster[:, 0].flatten().A[0], ptsInCurrCluster[:, 1].flatten().A[0], marker=markerStyle,
-                    s=90)
-    ax1.scatter(myCentroids[:, 0].flatten().A[0], myCentroids[:, 1].flatten().A[0], marker='+', s=300)
-    plt.show()
-
-
-def distSLC(vecA, vecB):
-    a = sin(vecA[0, 1] * pi / 180) * sin(vecB[0, 1] * pi / 180)
-    b = cos(vecA[0, 1] * pi / 180) * cos(vecB[0, 1] * pi / 180) * cos(pi * (vecB[0, 0] - vecA[0, 0]) / 180)
-    return arccos(a + b) * 6371.0
+# def clusterClubs(numClust=5):
+#     datList = []
+#     for line in open('places.txt').readlines():
+#         lineArr = line.split('\t')
+#         datList.append([float(lineArr[4]), float(lineArr[3])])
+#     datMat = mat(datList)
+#     myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+#     fig = plt.figure()
+#     rect = [0.1, 0.1, 0.8, 0.8]
+#     scatterMarkers = ['s', 'o', '^', '8', 'p', 'd', 'v', 'h', '>', '<']
+#     axprops = dict(xticks=[], yticks=[])
+#     ax0 = fig.add_axes(rect, label='ax0', **axprops)
+#     imgP = plt.imread('Portland.png')
+#     ax0.imshow(imgP)
+#     ax1 = fig.add_axes(rect, label='ax1', frameon=False)
+#     for i in range(numClust):
+#         ptsInCurrCluster = datMat[nonzero(clustAssing[:, 0].A == i)[0], :]
+#         markerStyle = scatterMarkers[i % len(scatterMarkers)]
+#         ax1.scatter(ptsInCurrCluster[:, 0].flatten().A[0], ptsInCurrCluster[:, 1].flatten().A[0], marker=markerStyle,
+#                     s=90)
+#     ax1.scatter(myCentroids[:, 0].flatten().A[0], myCentroids[:, 1].flatten().A[0], marker='+', s=300)
+#     plt.show()
+#
+#
+# def distSLC(vecA, vecB):
+#     a = sin(vecA[0, 1] * pi / 180) * sin(vecB[0, 1] * pi / 180)
+#     b = cos(vecA[0, 1] * pi / 180) * cos(vecB[0, 1] * pi / 180) * cos(pi * (vecB[0, 0] - vecA[0, 0]) / 180)
+#     return arccos(a + b) * 6371.0
 
 
 def drawPlot(datMat):
@@ -131,3 +131,8 @@ def drawPlot(datMat):
     ax.scatter(datMat[:, 0].flatten().A[0], datMat[:, 1].flatten().A[0], datMat[:, 2].flatten().A[0], marker='+', s=300)
     plt.show()
     return
+
+
+
+# dataMat = mat(kMeans.loadDataSet('testSet2.txt'))
+# kMeans.drawPlot(dataMat)
